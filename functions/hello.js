ccdd34031b516bb2245de4abf2026e10dd769647
Bastien P <bastien.pinard@ynov.com>
2020-04-24 10:03:43 +0200

    add functions

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,14 @@
 exports.handler = function (event, context, callback) {
+  console.log("yeeeeeeeeees")
+
+  console.log("queryStringParameters", event.queryStringParameters)
+
   callback(null, {
     statusCode: 200,
-    body: "Hello !",
+    body: JSON.stringify("Hello !"),
   })
 }
+
+//event -> declenche la fonction
+// callback -> gerer resultat et erreur
+// context -> info sur execution de notre environnement + allouer en fonction des ressources
