92a6490ab657f956edcf89ddf56de0a1e361566d
Bastien P <bastien.pinard@ynov.com>
2020-04-24 16:57:40 +0200

    add checkout

---
+++ src/pages/checkout/index.js
@@ -0,0 +1,25 @@
+import React from "react"
+import { loadStripe } from "@stripe/stripe-js"
+
+const Index = () => {
+  const stripe = loadStripe("pk_test_tT0WmGjmBsEhSCMihpSjmqH400njt8c3xJ")
+
+  const checkoutButton = async e => {
+    e.preventDefault()
+    const stripe = await stripePromise
+    stripe
+      .redirectToCheckout({
+        items: [{ sku: "sku_H9mSvNsxyF6iLS", quantity: 1 }],
+        successUrl: "",
+        cancelUrl: "",
+      })
+      .then(function (result) {})
+  }
+  return (
+    <div>
+      <button onClick={checkoutButton}>Checkout</button>
+    </div>
+  )
+}
+
+export default Index
