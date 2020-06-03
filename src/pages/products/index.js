0eac11d0b251aae390b47d187988ba31e000470a
Bastien P <bastien.pinard@ynov.com>
2020-06-03 23:02:49 +0200

    add snipcart 7

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -47,11 +47,11 @@ const Products = ({ data }) => {
                 <button
                   class="snipcart-add-item"
                   data-item-id="starry-night"
-                  data-item-price={product.node.frontmatter.price}
+                  data-item-price="79.99"
                   data-item-url="https://keen-yonath-4ea4f5.netlify.app/products"
-                  data-item-description={product.node.frontmatter.description}
+                  data-item-description="High-quality replica of The Starry Night by the Dutch post-impressionist painter Vincent van Gogh."
                   data-item-image={product.node.frontmatter.image}
-                  data-item-name={product.node.frontmatter.title}
+                  data-item-name="The Starry Night"
                 >
                   Add to cart
                 </button>
